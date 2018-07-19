aeea6fece0075bd100a2fb7d390c5711962fe784
박지용 <qwd330@naver.com>
2018-07-19 18:05:38 +0900

    SRT-40_박지용_로그 매니저

---
+++ RedTheSettlers/Assets/2.Scripts/Managers/LogManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogManager : MonoBehaviour {
+    public static LogManager GameLog;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="scriptName"></param>
+    /// <param name="text"></param>
+    public void WriteLog(string scriptName, string text)
+    {
+        text = DateTime.Now.ToString("yyyy.MM.dd-HH.mm.ss") + " [" + scriptName + "] " + "Log : " + text;
+
+    }
+
+    public void gd(string scriptName, string text)
+    {
+        Debug.Log("<color=green>" + text + "</color>");
+        WriteLog(scriptName, text);
+    }
+}
+
