adda9a72bd93007e4217172d866ae2cd69a4c1b8
박상원 <psw14com@gmail.com>
2018-07-20 18:38:42 +0900

    SRT-36 _ LogManager 완성도 90% + UI에 로그 시험용 TestCode는 LogManager 완성 후 삭제 예정

--- RedTheSettlers/Assets/2.Scripts/Managers/LogManager.cs
+++ RedTheSettlers/Assets/2.Scripts/Managers/LogManager.cs
@@ -1,26 +1,81 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class LogManager : MonoBehaviour {
-    public static LogManager GameLog;
+/// <summary>
+/// Log 기록을 전담
+/// Test 용도로 현재 IPointerClickHandler에 OnPointerClick 이벤트 사용중
+/// </summary>
+public class LogManager : Singleton<LogManager>, IPointerClickHandler
+{
+    //public static LogManager GameLog;
+    protected LogManager() { }
+
+    private FileStream logFile;
+    private FileInfo debugLog;
+    private DirectoryInfo folderCheck;
+
+    private string filename;
+    private string logText;
+
+    private void Awake()
+    {
+        filename = "Log-" + DateTime.Now.ToString("yyyy.MM.dd-HH.mm.ss") + ".txt";
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void CreateLogCheck(string scriptName, string text)
+    {
+        if (folderCheck == null)
+        {
+            CreateLog(scriptName, "Create and Test log Writing");
+            Debug.Log("새로 만들기");
+        }
+        else
+        {
+            WriteLog(scriptName, text);
+            Debug.Log("기존거에 내용 추가");
+        }
+    }
 
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="scriptName"></param>
-    /// <param name="text"></param>
-    public void WriteLog(string scriptName, string text)
+    /// <param name="scriptName">Log 기록을 요청한 Script의 이름</param>
+    /// <param name="text">기록할 로그 내용</param>
+    void CreateLog(string scriptName, string text)
     {
-        text = DateTime.Now.ToString("yyyy.MM.dd-HH.mm.ss") + " [" + scriptName + "] " + "Log : " + text;
+        folderCheck = new DirectoryInfo("./Log");
+        folderCheck.Create();
+        debugLog = new FileInfo("./log/" + filename);
 
+        if (!debugLog.Exists)
+        {
+            logFile = debugLog.Create();
+            logFile.Close();
+        }
+
+        logText = DateTime.Now.ToString("yyyy.MM.dd-HH.mm.ss") + " [" + scriptName + "] " + "Log : " + text;
+        File.AppendAllText("./log/" + filename, logText + "\n");
     }
 
-    public void gd(string scriptName, string text)
+    void WriteLog(string scriptName, string text)
+    {
+        logText = DateTime.Now.ToString("yyyy.MM.dd-HH.mm.ss") + " [" + scriptName + "] " + "Log : " + text;
+        File.AppendAllText("./log/" + filename, logText + "\n");
+    }
+
+    public void TestUserLog(string scriptName, string text)
     {
         Debug.Log("<color=green>" + text + "</color>");
-        WriteLog(scriptName, text);
+        CreateLogCheck(scriptName, text);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        CreateLogCheck("LogManager", "Test");
     }
 }
-
---
+++ RedTheSettlers/Assets/2.Scripts/UI/TestCode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestCode : MonoBehaviour,IPointerClickHandler
+{
+    LogManager logManager;
+    void Start ()
+    {
+        logManager = FindObjectOfType<LogManager>();
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        logManager.TestUserLog(GetType().Name, "잘 됩니까?");
+    }
+}
