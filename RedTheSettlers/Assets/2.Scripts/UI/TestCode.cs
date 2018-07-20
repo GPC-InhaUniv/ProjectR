f1442b229f4fd0ead188fa603855f6e1739a4b97
박상원 <psw14com@gmail.com>
2018-07-20 18:39:48 +0900

    주석문 추가

--- RedTheSettlers/Assets/2.Scripts/UI/TestCode.cs
+++ RedTheSettlers/Assets/2.Scripts/UI/TestCode.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// LogManager 테스트 완료 후 삭제 예정
+/// </summary>
 public class TestCode : MonoBehaviour,IPointerClickHandler
 {
     LogManager logManager;
