413ab86cb805789b54174248a0dda0f231becc43
박상원 <psw14com@gmail.com>
2018-07-25 18:41:00 +0900

    SRT-24 박상원 Input / InputManager 기본틀 잡는 중

---
+++ RedTheSettlers/Assets/2.Scripts/Managers/Input/InputManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputType
+{
+    Title,
+    Board,
+    Battle,
+    Trade,
+    Weather,
+}
+
+public class InputManager : Singleton<InputManager>
+{
+    private static InputManager inputManager;
+    private InputState input;
+    [SerializeField]
+    private InputType inputType;
+
+    private void Awake()
+    {
+        inputManager = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start ()
+    {
+        input = new TitleState();
+    }
+
+    /*void InputCurrentState()
+    {
+        //input.CurrentState();
+    }*/
+
+    void ChangeState(InputState inputState)
+    {
+        input = inputState;
+    }
+
+    void TypeState(InputType inputType)
+    {
+        switch(inputType)
+        {
+            case InputType.Board:
+                ChangeState(new BoardGameState());
+                break;
+            case InputType.Battle:
+                ChangeState(new BattlePhaseState());
+                break;
+            case InputType.Trade:
+                ChangeState(new TradeState());
+                break;
+            case InputType.Weather:
+                ChangeState(new WeatherState());
+                break;
+            case InputType.Title:
+                ChangeState(new TitleState());
+                break;
+        }
+    }
+}
