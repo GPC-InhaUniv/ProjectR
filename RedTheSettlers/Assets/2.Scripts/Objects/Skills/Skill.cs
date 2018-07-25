c74e47eb3ffa11ca5ed3e8aedf35eb3a8e1d7b9c
DulgiKim <dulgi0121@gmail.com>
2018-07-25 18:12:28 +0900

    SRT-32 김승환 스킬 작업(1차)

---
+++ RedTheSettlers/Assets/2.Scripts/Objects/Skills/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour {
+
+    private float duration = 0;
+
+    private IEnumerator Start()
+    {
+        while(duration > 3.0f)
+        {
+            yield return null;
+        }
+
+    }
+}
