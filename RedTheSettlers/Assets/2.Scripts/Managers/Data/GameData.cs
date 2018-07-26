3cbe201ce8e3df95f4a05827fb049ee864e58a17
박준명 <ggolnam3@gmail.com>
2018-07-26 17:51:35 +0900

    SRT-36 박준명 DataManager(1차)

---
+++ RedTheSettlers/Assets/2.Scripts/Managers/Data/GameData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Resource
+{
+    public int CowNum;
+    public int WaterNum;
+    public int WheatNum;
+    public int WoodNum;
+    public int IronNum;
+    public int SoilNum;
+}
+
+
+
+public class GameData
+{
+    
+    public const int maxHpLevel = 10;
+    public const int maxBulletLevel = 20;
+    public const int maxCritLevel = 20;
+    public const int maxTotalLevel = 50;
+
+    [Header("UserData")]
+    public int WeaponLevel;
+    public int ArmorLevel;
+    public int HaveTileNum;
+    public int BossKillCount;
+    
+
+    [Header("InGameData")]
+    public int TurnCount;
+    public int Weather;
+}
