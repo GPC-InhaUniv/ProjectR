9a4f09c77b66e6c8fa6ed29072ef42de4f4b1c2e
정진영 <qufqlaud@gmail.com>
2018-07-26 17:54:55 +0900

    SRT-67 정진영 Camera 작업 1단계(CameraController, UICamera)

---
+++ RedTheSettlers/Assets/2.Scripts/Camera/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject BattleCamera, BoardCamera;
+    public bool IsBoard;
+
+    private void Start()
+    {
+        BattleCamera = GameObject.Find("Battle Camera").GetComponent<GameObject>();
+        BoardCamera = GameObject.Find("Board Camera").GetComponent<GameObject>();
+    }
+
+    private void Update()
+    {
+        if (IsBoard)
+        {
+            ActiveFalse();
+            BoardCamera.SetActive(true);
+        }
+        else
+        {
+            ActiveFalse();
+            BattleCamera.SetActive(true);
+        }
+    }
+
+    void ActiveFalse()
+    {
+        BattleCamera.SetActive(false);
+        BoardCamera.SetActive(false);
+    }
+}
