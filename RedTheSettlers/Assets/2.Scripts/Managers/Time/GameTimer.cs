f9dce7e9d2ca547af1f94edf817e1cf537814aee
최대원 <chdw2438@gmail.com>
2018-07-26 16:07:05 +0900

    SRT-27 최대원 GameTimer수정 Timer Prefab추가

--- RedTheSettlers/Assets/2.Scripts/Managers/Time/GameTimer.cs
+++ RedTheSettlers/Assets/2.Scripts/Managers/Time/GameTimer.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+public delegate void TimerCallback();
 /// <summary>
-/// 타이머의 기반 클래스
+/// 타이머 클래스
 /// </summary>
-public class GameTimer
+public class GameTimer : MonoBehaviour
 {
-    //울릴 시간
     float snoozeTime;
-    //울리기 까지의 시간 카운트
-    float sumTime;
+    float elapseTime;
+
+    public TimerCallback Callback
+    {
+        get
+        {
+            return _callback;
+        }
+        set
+        {
+            _callback = value;
+        }
+    }
 
     bool isCounting = false;
     bool isRepeat = false;
+    private TimerCallback _callback;
 
     /// <summary>
     /// 타이머에 시간을 지정합니다. time : 시간, repeat : 반복 호출 여부
@@ -26,11 +36,11 @@ public class GameTimer
 
     /// <summary>
     /// 타이머를 처음부터 작동시킵니다.
-    /// </summary>
+    /// </summary>    
     public void StartTimer()
     {
         isCounting = true;
-        sumTime = 0f;
+        elapseTime = 0f;
     }
 
     /// <summary>
@@ -47,21 +57,20 @@ public class GameTimer
     public void StopTimer()
     {
         isCounting = false;
-        sumTime = 0f;
+        elapseTime = 0f;
     }
 
     private void Update()
     {
         if (isCounting)
         {
-            if (sumTime < snoozeTime)
+            if (elapseTime < snoozeTime)
             {
-                sumTime += GameTimeManager.DeltaTime;
+                elapseTime += GameTimeManager.DeltaTime;
             }
             else
             {
                 //알람이 울렸다.
-
                 if (isRepeat)
                 {
                     StartTimer();
@@ -70,6 +79,7 @@ public class GameTimer
                 {
                     StopTimer();
                 }
+                _callback();
             }
         }
     }
---
+++ RedTheSettlers/Assets/2.Scripts/Managers/Time/TestTimerScr.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class TestTimerScr : MonoBehaviour {
+
+    [SerializeField]
+    GameTimer timer;
+
+    GameObject skill_1_Obj;
+    GameTimer skill_1_Timer;
+    GameObject skill_2_Obj;
+    GameTimer skill_2_Timer;
+
+    private void Start()
+    {
+        skill_1_Obj = Instantiate(timer.gameObject);
+        skill_1_Timer = skill_1_Obj.GetComponent<GameTimer>();
+        skill_1_Timer.SetTimer(3.0f, true);
+        skill_1_Timer.Callback = new TimerCallback(execute);
+        skill_1_Timer.StartTimer();
+
+        skill_2_Obj = Instantiate(timer.gameObject);
+        skill_2_Timer = skill_2_Obj.GetComponent<GameTimer>();
+        skill_2_Timer.SetTimer(2.0f, true);
+        skill_2_Timer.Callback = new TimerCallback(execute2);
+        skill_2_Timer.StartTimer();
+    }
+
+    void execute()
+    {
+        Debug.Log("callback execute");
+    }
+
+    void execute2()
+    {
+        Debug.Log("callback execute2");
+    }
+}
