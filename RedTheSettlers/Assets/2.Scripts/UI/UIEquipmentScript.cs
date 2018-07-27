a86bbbed56594b8110152b92e9f8dd8c22dbc0e2
김하정 <hajeongkim1120@gmail.com>
2018-07-27 17:49:44 +0900

    SRT-46 김하정 장비UI 배치 그리고 스크립트 작업중

--- RedTheSettlers/Assets/2.Scripts/UI/UIEquipmentScript.cs
+++ RedTheSettlers/Assets/2.Scripts/UI/UIEquipmentScript.cs
@@ -1,11 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+    
 
 public class UIEquipmentScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private Image WeaponLevel1BlurImage;
+    [SerializeField]
+    private Image WeaponLevel2BlurImage;
+    [SerializeField]
+    private Image WeaponLevel3BlurImage;
+    [SerializeField]
+    private Image ShieldLevel1BlurImage;
+    [SerializeField]
+    private Image ShieldLevel2BlurImage;
+    [SerializeField]
+    private Image ShieldLevel3BlurImage;
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
