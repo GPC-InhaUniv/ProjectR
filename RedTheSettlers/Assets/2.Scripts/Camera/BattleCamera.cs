c2a859e422aa3a0c106cd68bb06759c8458d9bdd
정진영 <qufqlaud@gmail.com>
2018-08-01 17:42:21 +0900

    SRT-67 정진영 Camera작업 2-4(브릿지 패턴 설계중)

---
+++ RedTheSettlers/Assets/2.Scripts/Camera/BattleCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class BattleCamera : AbstractCamera
+{
+    public override void GetCamera()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //추상적 개념에 정의된 인터페이스를 확장한다
+    public override void MovingCamera()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ RedTheSettlers/Assets/2.Scripts/Camera/GameCamera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class GameCamera : MonoBehaviour
+{
+    [SerializeField]
+    AbstractCamera abstractCamera;
+    private void Start()
+    {
+        
+    }
+    //int CameraCount
+    //{
+    //    get
+    //    {
+    //        if(abstractCamera != null)
+    //        {
+    //            return abstractCamera.CameraModuleCount;
+    //        }
+    //        return -1;
+    //    }
+    //}
+    public void PutInCamera(AbstractCamera abstractCamera)
+    {
+        Debug.Log("게임카메라 풋인카메라"+abstractCamera);
+        this.abstractCamera = abstractCamera;
+    }
+    void TrunOnCamera()
+    {
+        abstractCamera.TurnOn();
+    }
+}
