f0e4b80ed0967d189249c6dd8ece528cdac5e7b0
DulgiKim <dulgi0121@gmail.com>
2018-08-02 14:46:16 +0900

    SRT-30 김승환 Player(Battle) 행동 양식 작성

--- RedTheSettlers/Assets/MoveSettingArea.cs
+++ RedTheSettlers/Assets/MoveSettingArea.cs
@@ -9,24 +9,29 @@ public class MoveSettingArea : MonoBehaviour {
 
     private void Start()
     {
-        StartCoroutine(MouseClick());
+        StartCoroutine(GetInput());
     }
 
-    private IEnumerator MouseClick()
+    private IEnumerator GetInput()
     {
         while(true)
         {
             if (Input.GetMouseButton(0))
             {
-                Move();
+                MoveByMousePointer();
                 yield return new WaitForSeconds(0.25f);
+            }
+
+            if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+            {
+                MoveByKeyboard();
             }
 
             yield return null;
         }
     }
 
-    private void Move()
+    private void MoveByMousePointer()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit[] hits = Physics.RaycastAll(ray);
@@ -38,14 +43,30 @@ public class MoveSettingArea : MonoBehaviour {
                 Vector3 targetPosition = hit.point + new Vector3(0, playerbattle.transform.position.y, 0);
                 if(coroutine == null)
                 {
-                    coroutine = StartCoroutine(playerbattle.MoveTo(targetPosition));
+                    coroutine = StartCoroutine(playerbattle.MoveToTargetPostion(targetPosition));
                 }
                 else
                 {
                     StopCoroutine(coroutine);
-                    coroutine = StartCoroutine(playerbattle.MoveTo(targetPosition));
+                    coroutine = StartCoroutine(playerbattle.MoveToTargetPostion(targetPosition));
                 }
             }
+        }
+    }
+
+    private void MoveByKeyboard()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (coroutine == null)
+        {
+            coroutine = StartCoroutine(playerbattle.MoveToDirection(direction));
+        }
+
+        else
+        {
+            StopCoroutine(coroutine);
+            coroutine = StartCoroutine(playerbattle.MoveToDirection(direction));
         }
     }
 }
