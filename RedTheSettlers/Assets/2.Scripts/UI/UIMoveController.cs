8e370a081a5e4af170436bcdab5fb703f6117495
dahee <isc1645@gmail.com>
2018-08-07 08:32:19 +0900

    SRT-51강다희 Player State UI 배치 변경 및 Script 'namespace' 추가작업

--- RedTheSettlers/Assets/2.Scripts/UI/UIMoveController.cs
+++ RedTheSettlers/Assets/2.Scripts/UI/UIMoveController.cs
@@ -4,38 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
-public class UIMoveController : MonoBehaviour
+namespace RedTheSettlers
 {
-    /// <summary>
-    /// 작성자 : 강다희
-    /// 플레이어의 현재 Move를 노출해주는 UI스크립트
-    ///
-    /// [중요] Turn, move, event를 Script 분리 할 것임
-    /// [중요] UI Manager 스크립트에서 각 상황에 맞게 처리 할 예정
-    /// </summary>
-
-    [Header("Player's Current Move")]
-    [SerializeField]
-    private Text moveText;
-
-    private int currentMove;
-
-    private void ChangeMove()
+    namespace UI
     {
-        currentMove = 5; //Test
+        public class UIMoveController : MonoBehaviour
+        {
+            /// <summary>
+            /// 작성자 : 강다희
+            /// 플레이어의 현재 Move를 노출해주는 UI스크립트
+            ///
+            /// [중요] Turn, move, event를 Script 분리 할 것임
+            /// [중요] UI Manager 스크립트에서 각 상황에 맞게 처리 할 예정
+            /// </summary>
 
-        moveText.text = currentMove.ToString();
+            [Header("Player's Current Move")]
+            [SerializeField]
+            private Text moveText;
 
-        //currentTurn.text = gameData.cowcow.ToString();
-        //이런식으로 현재 Move 정보를 가져옴.
-    }
+            private int currentMove;
 
-    private void Start()
-    {
-        ChangeMove();
-    }
+            private void ChangeMove()
+            {
+                currentMove = 5; //Test
 
-    private void Update()
-    {
+                moveText.text = currentMove.ToString();
+
+                //currentTurn.text = gameData.cowcow.ToString();
+                //이런식으로 현재 Move 정보를 가져옴.
+            }
+
+            private void Start()
+            {
+                ChangeMove();
+            }
+
+            private void Update()
+            {
+            }
+        }
     }
 }
--- RedTheSettlers/Assets/2.Scripts/UI/UIPlayerHoldResourceController.cs
+++ RedTheSettlers/Assets/2.Scripts/UI/UIPlayerHoldResourceController.cs
@@ -9,78 +9,84 @@ using UnityEngine.UI;
 /// [중요] UI Manager 스크립트에서 각 상황별로 처리할 예정.
 /// </summary>
 
-public class UIPlayerHoldResourceController : MonoBehaviour
+namespace RedTheSettlers
 {
-    [Header("Player's Total Resource")] //플레이어가 갖고 있는 자원 개수를 더한 값
-    [SerializeField]
-    private Text PlayerTotalResource;
-
-    [Header("Player's Resource")] //플레이어의 자원
-    [SerializeField]
-    private Text PlayerCowResource;
-
-    [SerializeField]
-    private Text PlayerWaterResource;
-
-    [SerializeField]
-    private Text PlayerWheatResource;
-
-    [SerializeField]
-    private Text PlayerWoodResource;
-
-    [SerializeField]
-    private Text PlayerIronResource;
-
-    [SerializeField]
-    private Text PlayerSoilResource;
-
-    [Header("Total Bar")] //플레이어의 total 자원을 더한 값을 bar 형식으로 보여줌
-    [SerializeField]
-    private Slider totalResourceBar;
-
-    private int cowCardNum;
-    private int waterCardNum;
-    private int wheatCardNum;
-    private int woodCardNum;
-    private int ironCardNum;
-    private int soilCardNum;
-    private float cardmaxNum;
-
-    private int computeResourceNum;
-
-    private void ChangeResource()
+    namespace UI
     {
-        cowCardNum = 10; //test
+        public class UIPlayerHoldResourceController : MonoBehaviour
+        {
+            [Header("Player's Total Resource")] //플레이어가 갖고 있는 자원 개수를 더한 값
+            [SerializeField]
+            private Text PlayerTotalResource;
 
-        PlayerCowResource.text = cowCardNum.ToString(); //test
+            [Header("Player's Resource")] //플레이어의 자원
+            [SerializeField]
+            private Text PlayerCowResource;
 
-        //PlayerCowResource.text = gameData.cowcow.ToString();
-        //이런식으로 6종류 자원을 gameData에서 가져와서 텍스트에 넣어줘야 함.
-    }
+            [SerializeField]
+            private Text PlayerWaterResource;
 
-    private void ComputeTotalResource() //플레이어가 갖고 있는 자원 개수를 모두 더하고, bar 형식으로 출력함
-    {
-        cowCardNum = Int32.Parse(PlayerCowResource.text);
-        waterCardNum = Int32.Parse(PlayerWaterResource.text);
-        wheatCardNum = Int32.Parse(PlayerWheatResource.text);
-        woodCardNum = Int32.Parse(PlayerWoodResource.text);
-        ironCardNum = Int32.Parse(PlayerIronResource.text);
-        soilCardNum = Int32.Parse(PlayerSoilResource.text);
+            [SerializeField]
+            private Text PlayerWheatResource;
 
-        computeResourceNum = cowCardNum + waterCardNum + wheatCardNum + woodCardNum + ironCardNum + soilCardNum;
-        PlayerTotalResource.text = computeResourceNum.ToString();
+            [SerializeField]
+            private Text PlayerWoodResource;
 
-        totalResourceBar.value = computeResourceNum / cardmaxNum;
-    }
+            [SerializeField]
+            private Text PlayerIronResource;
 
-    private void Start()
-    {
-        cardmaxNum = 50;
-        ChangeResource();
-        ComputeTotalResource();
-    }
+            [SerializeField]
+            private Text PlayerSoilResource;
 
-    private void Update()
-    {
+            [Header("Total Bar")] //플레이어의 total 자원을 더한 값을 bar 형식으로 보여줌
+            [SerializeField]
+            private Slider totalResourceBar;
+
+            private int cowCardNum;
+            private int waterCardNum;
+            private int wheatCardNum;
+            private int woodCardNum;
+            private int ironCardNum;
+            private int soilCardNum;
+            private float cardmaxNum;
+
+            private int computeResourceNum;
+
+            private void ChangeResource()
+            {
+                cowCardNum = 10; //test
+
+                PlayerCowResource.text = cowCardNum.ToString(); //test
+
+                //PlayerCowResource.text = gameData.cowcow.ToString();
+                //이런식으로 6종류 자원을 gameData에서 가져와서 텍스트에 넣어줘야 함.
+            }
+
+            private void ComputeTotalResource() //플레이어가 갖고 있는 자원 개수를 모두 더하고, bar 형식으로 출력함
+            {
+                cowCardNum = Int32.Parse(PlayerCowResource.text);
+                waterCardNum = Int32.Parse(PlayerWaterResource.text);
+                wheatCardNum = Int32.Parse(PlayerWheatResource.text);
+                woodCardNum = Int32.Parse(PlayerWoodResource.text);
+                ironCardNum = Int32.Parse(PlayerIronResource.text);
+                soilCardNum = Int32.Parse(PlayerSoilResource.text);
+
+                computeResourceNum = cowCardNum + waterCardNum + wheatCardNum + woodCardNum + ironCardNum + soilCardNum;
+                PlayerTotalResource.text = computeResourceNum.ToString();
+
+                totalResourceBar.value = computeResourceNum / cardmaxNum;
+            }
+
+            private void Start()
+            {
+                cardmaxNum = 50;
+                ChangeResource();
+                ComputeTotalResource();
+            }
+
+            private void Update()
+            {
+            }
+        }
     }
 }
