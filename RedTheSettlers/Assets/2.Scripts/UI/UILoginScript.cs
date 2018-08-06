8df473601ddad05d5a7e6254598c52147a91d0de
jun-PC\jun <ggolnam3@gmail.com>
2018-08-07 01:14:04 +0900

    SRT-36 박준명 DataManager 태스트를 위한 UILoginScript 수정

--- RedTheSettlers/Assets/2.Scripts/UI/UILoginScript.cs
+++ RedTheSettlers/Assets/2.Scripts/UI/UILoginScript.cs
@@ -16,14 +16,21 @@ namespace RedTheSettlers
             //[HideInInspector]
             [Header("Player Info")]
             [SerializeField]
-            private Text playerID;
+            private InputField playerID;
 
             //[HideInInspector]
             [SerializeField]
-            private Text playerPassword;
+            private InputField playerPassword;
 
             [SerializeField]
             private Text loginAlertText;
+
+            [SerializeField]
+            private InputField SignUpID;
+            [SerializeField]
+            private InputField SignUpPassword;
+            [SerializeField]
+            private InputField PasswordConfirmation;
 
             [SerializeField]
             private Text createAlertText;
@@ -31,7 +38,8 @@ namespace RedTheSettlers
             // Use this for initialization
             void Start()
             {
-                ChangeText();
+                loginAlertText.text = "정보가 올바르지 않습니다.";
+                createAlertText.text = "정보가 올바르지 않습니다.";
             }
 
             public void OnClickedGameQuitButton()
@@ -41,13 +49,37 @@ namespace RedTheSettlers
 
             public void OnClickedLoginButton()
             {
+                DataManager.Instance.Login(playerID.text, playerPassword.text);
                 ChangeText();
+            }
+
+            public void OnClickedCreateAccountButton()
+            {
+                if (SignUpPassword.text == PasswordConfirmation.text)
+                {
+                    DataManager.Instance.CreateNewAccount(SignUpID.text, SignUpPassword.text);
+                    ChangeText();
+                }
+                else
+                    createAlertText.text = "비밀번호확인이 일치하지 않습니다.";
+
+            }
+
+            public void onClickedUpdateButton()
+            {
+
+                DataManager.Instance.SaveGameData();
+            }
+
+            public void onClickedResetButton()
+            {
+                DataManager.Instance.ResetData();
             }
 
             private void ChangeText()
             {
-                loginAlertText.text = "정보가 올바르지 않습니다.";
-                createAlertText.text = "정보가 올바르지 않습니다.";
+                loginAlertText.text = "확인중.";
+                createAlertText.text = "확인중.";
             }
         }
     }
