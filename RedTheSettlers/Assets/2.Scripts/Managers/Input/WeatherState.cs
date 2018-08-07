eded88a87560da48b5e38f3801cbd447ff83583a
박상원 <psw14com@gmail.com>
2018-08-07 09:41:46 +0900

    SRT-24 박상원 InputManager / State 패턴 구현부 누락된 주석 추가

--- RedTheSettlers/Assets/2.Scripts/Managers/Input/WeatherState.cs
+++ RedTheSettlers/Assets/2.Scripts/Managers/Input/WeatherState.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 담당자 : 박상원
+/// 날씨 선택 화면 인풋 담당
+/// </summary>
 namespace RedTheSettlers
 {
     public class WeatherState : InputState
