58706318b42d3bbc5944865c7a3f981936bae548
최대원 <chdw2438@gmail.com>
2018-08-08 14:35:59 +0900

    최대원 네임 스페이스 누락 수정

--- RedTheSettlers/Assets/2.Scripts/Managers/Time/TestTimerScr.cs
+++ RedTheSettlers/Assets/2.Scripts/Managers/Time/TestTimerScr.cs
@@ -1,48 +1,43 @@
-﻿using RedTheSettlers.GameTime;
+﻿using RedTheSettlers.GameSystem;
 using UnityEngine;
 
-
-namespace RedTheSettlers
-{
-    namespace UnitTest
+namespace RedTheSettlers.UnitTest
+{ 
+    public class TestTimerScr : MonoBehaviour
     {
-        public class TestTimerScr : MonoBehaviour
+        [SerializeField]
+        GameTimer timer;
+
+        GameObject skill_1_Obj;
+        GameTimer skill_1_Timer;
+        GameObject skill_2_Obj;
+        GameTimer skill_2_Timer;
+
+        int count = 0;
+
+        private void Start()
         {
+            skill_1_Obj = Instantiate(timer.gameObject);
+            skill_1_Obj.transform.parent = transform;
+            skill_1_Timer = skill_1_Obj.GetComponent<GameTimer>();
+            skill_1_Timer.SetTimer(1.0f, true);
+            skill_1_Timer.Callback = new TimerCallback(execute);
+            skill_1_Timer.StartTimer();
+        }
 
-            [SerializeField]
-            GameTimer timer;
+        void execute()
+        {
+            //Debug.Log("callback execute");
+            skill_2_Timer = GameTimeManager.Instance.PopTimer();
+            skill_2_Timer.SetTimer(1f, false);
+            skill_2_Timer.Callback = new TimerCallback(callback);
+            skill_2_Timer.StartTimer();
+        }
 
-            GameObject skill_1_Obj;
-            GameTimer skill_1_Timer;
-            GameObject skill_2_Obj;
-            GameTimer skill_2_Timer;
-
-            int count = 0;
-
-            private void Start()
-            {
-                skill_1_Obj = Instantiate(timer.gameObject);
-                skill_1_Obj.transform.parent = transform;
-                skill_1_Timer = skill_1_Obj.GetComponent<GameTimer>();
-                skill_1_Timer.SetTimer(1.0f, true);
-                skill_1_Timer.Callback = new TimerCallback(execute);
-                skill_1_Timer.StartTimer();
-            }
-
-            void execute()
-            {
-                //Debug.Log("callback execute");
-                skill_2_Timer = GameTimeManager.Instance.PopTimer();
-                skill_2_Timer.SetTimer(1f, false);
-                skill_2_Timer.Callback = new TimerCallback(callback);
-                skill_2_Timer.StartTimer();
-            }
-
-            void callback()
-            {
-                Debug.Log("callback execute : " + count);
-                count++;
-            }
+        void callback()
+        {
+            Debug.Log("callback execute : " + count);
+            count++;
         }
     }
 }
