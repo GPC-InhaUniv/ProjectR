cf3dd287040e143e81fac2d4f21be77afc06c03c
DulgiKim <dulgi0121@gmail.com>
2018-08-08 16:03:09 +0900

    김승환 네임스페이스 수정

--- RedTheSettlers/Assets/2.Scripts/Objects/Skills/SkillController.cs
+++ RedTheSettlers/Assets/2.Scripts/Objects/Skills/SkillController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using RedTheSettlers.System;
+using RedTheSettlers.GameSystem;
 
 namespace RedTheSettlers
 {
--- RedTheSettlers/Assets/3.Prefabs/Skills/PyroParticles/Prefab/Script/FireBaseScript.cs
+++ RedTheSettlers/Assets/3.Prefabs/Skills/PyroParticles/Prefab/Script/FireBaseScript.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-using RedTheSettlers.System;
+using RedTheSettlers.GameSystem;
 
 namespace DigitalRuby.PyroParticles
 {
