b70b49408923a76eb82a5c4bd0bf46688352cecb
박지용 <qwd330@naver.com>
2018-08-08 15:27:39 +0900

    SRT-41 박지용 데이터 구조 변경

--- RedTheSettlers/Assets/1.Scenes/UnitTestScenes/TradeControllerTest/TradeControllerTest.cs
+++ RedTheSettlers/Assets/1.Scenes/UnitTestScenes/TradeControllerTest/TradeControllerTest.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RedTheSettlers.GameSystem;
 
 namespace RedTheSettlers.UnitTest
 {
-
     abstract public class Player
     {
         private IMediatable mediator;
-        public ResourceData tradeData;
+        public ItemData tradeData;
         public GameObject tradePanel;
 
         public Player(IMediatable mediator)
@@ -19,14 +19,14 @@ namespace RedTheSettlers.UnitTest
         /// <summary>
         /// 거래 패널을 보여준다. 사용자가 거래 조건을 설정하면 패널을 종료하고 해당 자원 정보를 구조체로 만들어서 전달한다.
         /// </summary>
-        public ResourceData ShowTradePanel()
+        public ItemData ShowTradePanel()
         {
             tradePanel.SetActive(true);
             // TradePanel에서 거래 내용 조작
             // 조작이 끝나고 거래를 요청하면 
 
             // 아래 코드는 정상적으로 동작하지 않을것으로 예상됨. 나중에 다른 메소드로 분리할것
-            ResourceData sendData = GetTradeData();
+            ItemData sendData = GetTradeData();
             tradePanel.SetActive(false);
             return sendData;
         }
@@ -35,12 +35,12 @@ namespace RedTheSettlers.UnitTest
         /// UI에서 해줘야하는 작업. 임시로 만듬
         /// 교환할 자원 정보를 구조체로 만들어서 전달
         /// </summary>
-        public ResourceData GetTradeData()
+        public ItemData GetTradeData()
         {
-            ResourceData sendData = new ResourceData
+            ItemData sendData = new ItemData
             {
-                CowNum = 1,
-                WaterNum = -2
+                CowNumber = 1,
+                WaterNumber = -2
             };
             return sendData;
         }
@@ -50,14 +50,14 @@ namespace RedTheSettlers.UnitTest
         /// </summary>
         /// <param name="requestPlayer">거래를 요청한 플레이어</param>
         /// <param name="tradeData">요구하는 자원 거래 정보</param>
-        public void RequestTrade(Player requestPlayer, ResourceData tradeData)
+        public void RequestTrade(Player requestPlayer, ItemData tradeData)
         {
-            ResourceData sendData = ShowTradePanel();
+            ItemData sendData = ShowTradePanel();
             mediator.RequestTrade(requestPlayer, tradeData);
         }
 
 
-        public void SendTrade(Player requestPlayer, ResourceData tradeData)
+        public void SendTrade(Player requestPlayer, ItemData tradeData)
         {
             tradePanel.SetActive(true);
             // tradeData를 풀어 UI에서 정보 보여주기
@@ -66,7 +66,7 @@ namespace RedTheSettlers.UnitTest
             mediator.RequestAgain(requestPlayer, tradeData);
         }
 
-        public void RequestAgain(Player requestPlayer, ResourceData tradeData)
+        public void RequestAgain(Player requestPlayer, ItemData tradeData)
         {
             mediator.RequestAgain(requestPlayer, tradeData);
         }
@@ -84,8 +84,8 @@ namespace RedTheSettlers.UnitTest
         public interface IMediatable
         {
             void RegisterPlayer(Player player);
-            void RequestTrade(Player requestPlayer, ResourceData tradeData);
-            void RequestAgain(Player respondPlayer, ResourceData tradeData);
+            void RequestTrade(Player requestPlayer, ItemData tradeData);
+            void RequestAgain(Player respondPlayer, ItemData tradeData);
             //void ShowTradePanel(Player requestPlayer);
         }
 
@@ -113,7 +113,7 @@ namespace RedTheSettlers.UnitTest
                 playerList.Add(player);
             }
 
-            public void RequestTrade(Player requestPlayer, ResourceData tradeData)
+            public void RequestTrade(Player requestPlayer, ItemData tradeData)
             {
                 for (int i = 0; i < playerList.Count; i++)
                 {
@@ -122,7 +122,7 @@ namespace RedTheSettlers.UnitTest
                 }
             }
 
-            public void RequestAgain(Player requestPlayer, ResourceData tradeData)
+            public void RequestAgain(Player requestPlayer, ItemData tradeData)
             {
                 for (int i = 0; i < playerList.Count; i++)
                 {
