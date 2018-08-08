79742434f5d343ccb639deb438dd6f91b1d2e8e1
최대원 <chdw2438@gmail.com>
2018-08-08 15:55:11 +0900

    최대원 네임 스페이스 추가 수정

--- RedTheSettlers/Assets/2.Scripts/Managers/GameManager.cs
+++ RedTheSettlers/Assets/2.Scripts/Managers/GameManager.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
 
-namespace RedTheSettlers
+namespace RedTheSettlers.GameSystem
 {
-    namespace System
+    /// <summary>
+    /// 각 컨트롤러를 관리하고 중재하는 매니저
+    /// </summary>
+    public class GameManager : Singleton<GameManager>
     {
-        /// <summary>
-        /// 각 컨트롤러를 관리하고 중재하는 매니저
-        /// </summary>
-        public class GameManager : MonoBehaviour
-        {
-            /*
-            각 컨트롤러 참조, 관리
-            데이터, 인풋 매니저로부터 요청을 받음
-            요청을 실행하거나 다른 매니저에게 전달
-            각 플레이어(보드4 + 전투1)를 가지고 있음
-            */
-        }
+
+        /*
+        각 플레이어(보드4 + 전투1)를 가지고 있음
+
+        컨트롤러로 부터 작업이 끝났다는 요청를 받는다.
+        다음 컨트롤러로 작업을 넘겨준다.
+        이 과정에서 데이터와 연동
+        
+         */
+
     }
 }
 
