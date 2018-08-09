607e9a166d1ea0a4cf37d5239acf6b9ee469c8d2
DulgiKim <dulgi0121@gmail.com>
2018-08-09 13:47:31 +0900

    김승환 전역변수 및 열거형 CS 파일 생성

---
+++ RedTheSettlers/Assets/2.Scripts/GlobalVariable/GlobalVariable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Cow = 0,
+    Iron = 1,
+    Soil = 2,
+    Water = 3,
+    Wheat = 4,
+    Wood = 5,
+}
+
+static class GlobalVariables
+{
+    public const int tileGridSize = 9;
+    public const int minZIntercept = 3;
+    public const int maxZIntercept = 13;
+}
