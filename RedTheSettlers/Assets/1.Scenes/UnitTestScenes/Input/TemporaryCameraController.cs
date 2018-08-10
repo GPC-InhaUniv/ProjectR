78cdf7e9e1a5d01089a5bf39f0c1b98fd6a92732
박상원 <psw14com@gmail.com>
2018-08-10 18:13:45 +0900

    SRT-24 박상원 InputManager 카메라 줌인 구현 , 테스트 스크립트 추가

---
+++ RedTheSettlers/Assets/1.Scenes/UnitTestScenes/Input/TemporaryCameraController.cs
@@ -0,0 +1,24 @@
+﻿using RedTheSettlers.GameSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryCameraController : MonoBehaviour
+{
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    public void CameraMove(Vector3 direction)
+    {
+        camera.transform.Translate(new Vector3(direction.x, 0, direction.y), Space.World);
+    }
+
+    public void CameraZoom(float value)
+    {
+        camera.fieldOfView += value;
+    }
+}
--- RedTheSettlers/Assets/1.Scenes/UnitTestScenes/Input/TemporaryGameManager.cs
+++ RedTheSettlers/Assets/1.Scenes/UnitTestScenes/Input/TemporaryGameManager.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 
 public class TemporaryGameManager : Singleton<TemporaryGameManager>
 {
-    private Transform transformCamera;
+    //private Transform transformCamera;
+    private Camera camera;
+    private TemporaryCameraController tempCameraController;
     //private new Transform Player;
     private new Rigidbody Player;
     //public Vector3 moveDirection;
 
     private void Awake()
     {
-        transformCamera = GameObject.FindWithTag("MainCamera").GetComponent<Transform>();
+        //transformCamera = GameObject.FindWithTag("MainCamera").GetComponent<Transform>();
+        //camera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        tempCameraController = GameObject.FindObjectOfType<TemporaryCameraController>();
         //Player = GameObject.FindWithTag("Player").GetComponent<Transform>();
         //Player = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
         DontDestroyOnLoad(gameObject);
@@ -27,13 +31,17 @@ public class TemporaryGameManager : Singleton<TemporaryGameManager>
 
     public void CameraMove(Vector3 direction)
     {
-        transformCamera.transform.Translate(new Vector3(direction.x,0,direction.y),Space.World);
-        LogManager.Instance.UserDebug(LogColor.Blue, GetType().Name, "이동 좌표 : " + direction);
+        //transformCamera.transform.Translate(new Vector3(direction.x,0,direction.y),Space.World);
+        //camera.transform.Translate(new Vector3(direction.x, 0, direction.y), Space.World);
+        tempCameraController.CameraMove(direction);
+        //LogManager.Instance.UserDebug(LogColor.Blue, GetType().Name, "이동 좌표 : " + direction);
     }
 
     public void CameraZoom(float value)
     {
-        transformCamera.transform.Translate(new Vector3(0, value, 0),Space.World);
+        //transformCamera.transform.Translate(new Vector3(0, value, 0),Space.World);
+        //camera.fieldOfView += value;
+        tempCameraController.CameraZoom(value);
     }
 
     public void UserTrade(Vector3 position)
