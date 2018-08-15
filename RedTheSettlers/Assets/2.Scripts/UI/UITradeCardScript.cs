ef33766edb362d1b93cddd265ce8d091ea3ae84b
hajeong-laptop\hajeong <hajeongkim1120@gmail.com>
2018-08-15 16:25:08 +0900

    SRT-48 김하정 교환UI 재구현 시작

---
+++ RedTheSettlers/Assets/2.Scripts/UI/UITradeCardScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITradeCardScript : MonoBehaviour
+{
+
+    private Vector3 mousePos;
+    // Use this for initialization 
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        
+    }
+ 
+
+}
