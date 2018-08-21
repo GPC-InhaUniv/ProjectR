819cd779702eedaf72653f1cf239a02c6315d25e
이재환 <ok17jh@gmail.com>
2018-08-21 19:21:38 +0900

    SRT-22 이재환 StageManager (오늘내일 끝내자)

--- RedTheSettlers/Assets/2.Scripts/Managers/StageManager/StageStateMachine.cs
+++ RedTheSettlers/Assets/2.Scripts/Managers/StageManager/StageStateMachine.cs
@@ -2,18 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageStateMachine : MonoBehaviour
+/// <summary>
+/// 담당자 : 이재환
+/// 수정시 간략 설명과 수정 날짜 
+/// {
+///   Ex : 함수명 변경 18/07/15
+///   Context 부분 
+///   
+/// }
+/// </summary>
+
+
+namespace RedTheSettlers.GameSystem
 {
-
-    // Use this for initialization
-    void Start()
+    public class StageStateMachine
     {
+        private State currentstate;
+        public State CurrentState
+        {
+            get { return currentstate; }
+        }
 
-    }
+        public void Enter()
+        {
+            currentstate.Enter();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-
+        public void Exit()
+        {
+            currentstate.Exit();
+        }
+                
     }
 }
+
--- RedTheSettlers/Assets/2.Scripts/Managers/StageManager/TitleState.cs
+++ RedTheSettlers/Assets/2.Scripts/Managers/StageManager/TitleState.cs
@@ -16,15 +16,17 @@ namespace RedTheSettlers.GameSystem
 {
     class TitleState : State
     {
-                
         public override State Enter()
         {
             throw new System.NotImplementedException();
+            //확인 버튼을 누르면
+
         }
 
         public override State Exit()
         {
             throw new System.NotImplementedException();
+            // 로그인이 성공이라면 로딩장면 상태로 넘어간다.
         }
     }
 
