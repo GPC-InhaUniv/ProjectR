8c93efeaed35fe18e333c554b3c8b8020f21745d
최대원 <chdw2438@gmail.com>
2018-08-22 18:21:57 +0900

    SRT-31 최대원 Boss 공격과 스킬 구현중

--- RedTheSettlers/Assets/2.Scripts/Objects/Enemy/Skill/Explode.cs
+++ RedTheSettlers/Assets/2.Scripts/Objects/Enemy/Skill/Explode.cs
@@ -5,22 +5,34 @@ namespace RedTheSettlers.GameSystem
     public class Explode : MonoBehaviour
     {
         [SerializeField]
-        private GameObject SkillRangeCircle;
-        private ParticleSystem particle;
-        private float skillRange;
+        public GameObject SkillRangeCircle;
+        [SerializeField]
+        public ParticleSystem particle;
+        public bool isViewingCircle;
+        public SphereCollider SphereColliderConponent;
+        public float skillRange;
 
         private void Start()
         {
-            particle = GetComponent<ParticleSystem>();
-            skillRange = GetComponent<SphereCollider>().radius / 5;
+            particle = GetComponentInChildren<ParticleSystem>();
+            SphereColliderConponent = GetComponentInChildren<SphereCollider>();
+            Setting(1);
+        }
+
+        public void Setting(float radius)
+        {
+            SphereColliderConponent.radius = radius + 1;
+            skillRange = SphereColliderConponent.radius / 5;
             SkillRangeCircle.transform.localScale = new Vector3(skillRange, 0f, skillRange);
             SkillRangeCircle.SetActive(false);
+            isViewingCircle = false;
+            particle.gameObject.SetActive(false);
             gameObject.SetActive(false);
         }
 
         private void Update()
         {
-            if (!particle.isEmitting)
+            if (!isViewingCircle && !particle.isEmitting)
             {
                 SkillRangeCircle.SetActive(false);
             }
--- RedTheSettlers/Assets/2.Scripts/Objects/Enemy/State/BossEnemyState/Attack.cs
+++ RedTheSettlers/Assets/2.Scripts/Objects/Enemy/State/BossEnemyState/Attack.cs
@@ -9,20 +9,27 @@ namespace RedTheSettlers.Enemys.Boss
         private float bossPhase;
         private GameTimer fireballTimer;
         private TimerCallback timerCallback;
+        private const float shotOffset = 1.2f;
 
-        public Attack(Animator animator, EnemyFireBall fireBall, Vector3 fireBallPosition, float bossPhase, GameTimer fireballTimer, TimerCallback timerCallback)
+        public Attack(Animator animator, EnemyFireBall fireBall, Vector3 fireBallPosition, float bossPhase, GameTimer fireballTimer, TimerCallback timerCallback, GameObject targetObject, Transform transform, float TimeToReturn)
         {
             this.animator = animator;
             this.fireBall = fireBall;
             this.fireBallPosition = fireBallPosition;
             this.fireballTimer = fireballTimer;
             this.timerCallback = timerCallback;
+            this.targetObject = targetObject;
+            this.transform = transform;
+            this.timeToReturn = TimeToReturn;
         }
 
         public override void DoAction()
         {
             animator.SetTrigger("Attack");
-
+            fireBall.gameObject.transform.position = transform.rotation * Vector3.forward * shotOffset + transform.position;
+            fireBall.gameObject.transform.rotation = transform.rotation;
+            fireballTimer = GameTimeManager.Instance.PopTimer();
+            fireballTimer.SetTimer(timeToReturn, false);
         }
     }
 }
