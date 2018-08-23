716b49a34184cb42c14d01588c39cb0955e7da57
dahee <isc1645@gmail.com>
2018-08-23 18:27:02 +0900

    SRT-53 강다희 UI GameResult 스크립트 작업중

---
+++ RedTheSettlers/Assets/2.Scripts/UI/UIGameResult.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 작성자 : 강다희
+/// game 결과에 따라서,문구 출력 및, 버튼 이벤트
+/// </summary>
+
+namespace RedTheSettlers.UI
+{
+    public class UIGameResult : MonoBehaviour
+    {
+        [Header("Result Text")]
+        [SerializeField]
+        private Text gameResultText;
+
+        [Header("Enter Scene Button")]
+        [SerializeField]
+        private GameObject MainSceneButton;
+
+        [SerializeField]
+        private GameObject CloseSceneButton;
+
+        public void ChangeResultText()
+        {
+        }
+
+        public void OnEnterScene()
+        {
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
