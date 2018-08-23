365f86602eb0eb7158cd292c9c15b4c9996fa8f9
qwo330 <qwd330@naver.com>
2018-08-24 01:36:15 +0900

    SRT- 94 박지용 TradeUI 수정중

--- RedTheSettlers/Assets/2.Scripts/UI/UIManager.cs
+++ RedTheSettlers/Assets/2.Scripts/UI/UIManager.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : Singleton<UIManager>
-{
-    [SerializeField]
-    private GameObject gameResultCalculateUI;
-
-    [SerializeField]
-    private GameObject equipmentAndSkillUI;
-
-    private void Start()
+namespace RedTheSettlers.GameSystem {
+    /// <summary>
+    /// 작성자 : 
+    /// </summary>
+    public class UIManager : Singleton<UIManager>
     {
-        equipmentAndSkillUI.gameObject.SetActive(true);
-    }
+        [SerializeField]
+        private GameObject gameResultCalculateUI;
 
-    public void OnClickedBuildCamp()
-    {
-        gameResultCalculateUI.gameObject.SetActive(true);
+        [SerializeField]
+        private GameObject equipmentAndSkillUI;
+
+        private void Start()
+        {
+            equipmentAndSkillUI.gameObject.SetActive(true);
+        }
+
+        public void OnClickedBuildCamp()
+        {
+            gameResultCalculateUI.gameObject.SetActive(true);
+        }
+
+        /// <summary>
+        /// 작성자 : 박지용
+        /// TradeUI 패널에서 가져온 tradeData를 반환하는 함수
+        /// </summary>
+        public TradeData GetTradeData()
+        {
+            TradeData tradeData = new TradeData();
+            return tradeData;
+        }
     }
 }
