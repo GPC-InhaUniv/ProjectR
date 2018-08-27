32ff53809d3134a05d51de4205c7708e887c84d4
dahee <isc1645@gmail.com>
2018-08-27 15:03:13 +0900

    SRT-53 강다희 게임 클리어/오버 결과 스크립트 작성

--- RedTheSettlers/Assets/2.Scripts/UI/UIGameResult.cs
+++ RedTheSettlers/Assets/2.Scripts/UI/UIGameResult.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 /// <summary>
 /// 작성자 : 강다희
 /// game 결과에 따라서,문구 출력 및, 버튼 이벤트
+/// [참고]UIManager에서 게임결과 text 연결
 /// </summary>
 
 namespace RedTheSettlers.UI
@@ -23,16 +24,34 @@ namespace RedTheSettlers.UI
         [SerializeField]
         private GameObject CloseSceneButton;
 
+        private int resultType;
+
         public void ChangeResultText()
         {
+            if (resultType == 0)
+            {
+                gameResultText.text = "Game Clear";
+            }
+            else
+            {
+                gameResultText.text = "Game Over";
+            }
         }
 
         public void OnEnterScene()
         {
+            //스테이지 매니저 쪽에서 넘겨줘야 함.
+        }
+
+        public void CloseScene()
+        {
+            //스테이지 매니저 쪽에서 넘겨줘야 함.
         }
 
         private void Start()
         {
+            resultType = 1; //유아이 매니저쪽에서 넘겨줘야 함.
+            ChangeResultText();
         }
 
         private void Update()
