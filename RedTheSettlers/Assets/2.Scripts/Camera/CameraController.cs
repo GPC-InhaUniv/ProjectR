a8ab606ed12ca5c9a819aea6e41d1c81cd2d8a1d
정진영 <qufqlaud@gmail.com>
2018-08-30 05:51:25 +0900

    SRT-67 정진영 Camera작업 (CameraSwiching 할 때 타일 배치가 끝났는지 안끝났는지 확인하는거 추가)

--- RedTheSettlers/Assets/2.Scripts/Camera/CameraController.cs
+++ RedTheSettlers/Assets/2.Scripts/Camera/CameraController.cs
@@ -42,6 +42,7 @@ namespace RedTheSettlers.GameSystem
         [SerializeField]
         Vector3 playerVector3;
         Transform playerTransform;
+        bool ConsentToSwiching = false;
         //float ZoomValue;
         //Vector3 vector3; //배틀 카메라를 사용할때 비어있는 v3를 전달하기위해 선언함
 
@@ -168,6 +169,12 @@ namespace RedTheSettlers.GameSystem
             }
         }
 
+        public void GiveConsentToSwiching(bool pass)
+        {
+            if (pass) ConsentToSwiching = true;
+            else ConsentToSwiching = false;
+        }
+
         /// <summary>
         /// 카메라 스위칭할때 애니메이션, 싱크
         /// StageManager와 연관
@@ -194,107 +201,18 @@ namespace RedTheSettlers.GameSystem
             ActiveCamera.TrunOnCamera();
 
             //여기에서 배틀타일이 완성되었는지 판단하고 다음을 진행한다
-            while (true)
+            while (ActiveCamera==BattleGameCamera)
             {
-                if (true)
+                if (ConsentToSwiching == true)
                 {
                     break;
                 }
+                yield return new WaitForSeconds(1f);
             }
             cloudAnimator.SetTrigger("Opening");
             cloudAnimator.SetTrigger("Idle");
 
             yield return null;
         }
-
-
-
-
-
-        //드레그카메라에 들어가는 vector3을 계산하는 조건
-        //    //Debug.Log("보드카메라 무빙카메라()");
-        //        if (Input.GetMouseButtonDown(0))
-        //        {
-        //            dragOrigin = Input.mousePosition;
-        //            return;
-        //        }
-        //        if (!Input.GetMouseButton(0)) return;
-        //        Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
-        //Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-
-
-
-        //public Camera BattleCamera, BoardCamera, ActiveCamera;
-        //GameCamera gameCamera;
-        //public bool IsZoom = false;
-        //public float ZoomSpeed = 15f;
-        //private void Start()
-        //{
-        //    gameCamera = new GameCamera();
-        //    BoardCameraTest(gameCamera);
-
-        //    BoardCamera = GameObject.Find("Board Camera").GetComponent<Camera>();
-        //    BattleCamera = GameObject.Find("Battle Camera").GetComponent<Camera>();
-
-        //    //BoardCamera.enabled = false;
-        //    //BattleCamera.enabled = false;
-        //    //InitializeingCamera();
-        //}
-        //void BoardCameraTest(GameCamera gameCamera)
-        //{
-
-        //    //ICamera module = new ~();
-        //    AbstractCamera abstractCamera = new BoardCamera();
-        //    //abstractCamera.addModule(module);
-        //    //gameCamera.putInCamera(abstractCamera);
-        //    BoardCamera = abstractCamera.camera;
-
-        //}
-
-        //private void Update()
-        //{
-
-        //    //if (Input.GetKeyDown(KeyCode.X))
-        //    //{
-        //    //    StartCoroutine(CameraTransition());
-        //    //}
-        //    //if (IsZoom==true)
-        //    //{
-        //    //    BoardCamera br = ActiveCamera.GetComponent<BoardCamera>();
-        //    //    br.ZoomInOut(IsZoom);
-        //    //}
-        //}
-
-        //void InitializeingCamera()
-        //{
-        //    ActiveCamera = BoardCamera;
-        //    ActiveCamera.enabled = true;
-        //}
-
-
-
-        //IEnumerator CameraTransition()
-        //{
-        //    Debug.Log("코루틴");
-        //    //while (true)
-        //    //{
-        //    //    if (BoardCamera.fieldOfView > 1)
-        //    //    {
-        //    //        BoardCamera.fieldOfView -= ZoomSpeed;
-        //    //        yield return new WaitForSeconds(0.06f);
-        //    //    }
-        //    //    else
-        //    //    {
-        //    //        break;
-        //    //    }
-        //    //}
-        //    //yield return new WaitForSeconds(1f);
-        //    SwichingCamera(ActiveCamera);
-        //    Debug.Log("애니메이션끝");
-        //    yield return new WaitForSeconds(1f);
-        //}
-
-
-
     } 
 }
