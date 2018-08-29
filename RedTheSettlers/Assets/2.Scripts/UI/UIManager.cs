eb8e4e18bda03da1e8b161d947da0dc8767f92a9
dahee <isc1645@gmail.com>
2018-08-29 23:49:15 +0900

    SRT-92 강다희 UIManager 수정

--- RedTheSettlers/Assets/2.Scripts/UI/UIManager.cs
+++ RedTheSettlers/Assets/2.Scripts/UI/UIManager.cs
@@ -17,6 +17,9 @@ namespace RedTheSettlers.UI
         private GameObject playerTurnUI;
 
         [SerializeField]
+        private GameObject CommonPlayerItemUI;
+
+        [SerializeField]
         private GameObject tradeUI, equipmentAndSkillUI, stateUI;
 
         [SerializeField]
@@ -31,9 +34,10 @@ namespace RedTheSettlers.UI
         [SerializeField]
         private GameObject selectTileUI;
 
-        private void ShowBoardUI()
+        public void ShowBoardUI()
         {
             playerTurnUI.SetActive(true);
+            CommonPlayerItemUI.SetActive(true);
         }
 
         public void OnClickedEquipAndSkillButton()
