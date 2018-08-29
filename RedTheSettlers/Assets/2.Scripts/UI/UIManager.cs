8ed840714816f12b1257a915268d1067ef9b16f0
김하정 <hajeongkim1120@gmail.com>
2018-08-30 02:26:28 +0900

    Revert "SRT-92 김하정 유아이매니저 연결"
    This reverts commit 9dabc54b0b533de0d7fc50f76aab1268aa32eb54.

--- RedTheSettlers/Assets/2.Scripts/UI/UIManager.cs
+++ RedTheSettlers/Assets/2.Scripts/UI/UIManager.cs
@@ -129,11 +129,8 @@ namespace RedTheSettlers.UI
         /// </summary>
         public void SendPlayerItems(int wood, int iron, int soil)
         {
-            GameManager gameManager = GameManager.Instance;
-
-            gameManager.AddItemByType(0, ItemType.Wood, wood);
-            gameManager.AddItemByType(0, ItemType.Iron, iron);
-            gameManager.AddItemByType(0, ItemType.Soil, soil);
+            //게임 매니저에 넣을 수 있는 함수
+            //AddItemByType
         }
     }
 }
