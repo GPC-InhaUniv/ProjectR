39d405c3266d8ac1531813cc7ad8cc4713f84576
김하정 <hajeongkim1120@gmail.com>
2018-08-29 15:10:08 +0900

    SRT-85 김하정 로그인 씬 합침

---
+++ RedTheSettlers/Assets/2.Scripts/UI/UIShowItemCards.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RedTheSettlers.GameSystem;
+using UnityEngine.UI;
+
+namespace RedTheSettlers.UI
+{
+    public class UIShowItemCards : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject cardObject;
+
+        // Use this for initialization
+        void Start() {
+            //MakeCard();
+        }
+
+        GameData gameData;
+
+        private void MakeCard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                GameObject card = Instantiate(cardObject) as GameObject;
+            }
+            //Debug.Log(gameData.PlayerData[0].TileList.Count);
+        }
+    }
+}
