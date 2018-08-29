355602285778c93178a7ff9108c34266205b78cd
박지용 <qwd330@naver.com>
2018-08-30 01:58:30 +0900

    SRT-25 박지용 턴 컨트롤러 SRT-26 박지용 배틀 컨트롤러

--- RedTheSettlers/Assets/2.Scripts/UnitTest/TurnControllerTest/TurnControllerTest.cs
+++ RedTheSettlers/Assets/2.Scripts/UnitTest/TurnControllerTest/TurnControllerTest.cs
@@ -6,16 +6,6 @@ using RedTheSettlers.Users;
 
 namespace RedTheSettlers.UnitTest
 {
-    public enum GameState
-    {
-        EventController,
-        ItemController,
-        PlayerTurn,
-        AI1Turn,
-        AI2Turn,
-        AI3Turn,
-    }
-
     /// <summary>
     /// 작성자 : 박지용
     /// 유저와 AI의 턴 흐름을 제어한다.
@@ -36,25 +26,32 @@ namespace RedTheSettlers.UnitTest
             get{ return _callback; }
             set { _callback = value; }
         }
-        
+
         public void TurnFlow(GameState gameState)
         {
-            CurrentPlayerTurn = (gameState - GameState.AI1Turn + 1);
-
-            SendGameLog();
+            if (gameState == GameState.PlayerTurn)
+            {
+                // 플레이어가 타일을 이동하고 전투를 시작하면 gameManager에서 battleFlow 호출
+                // 정확히는 diffcultyController가 끝나면 battleFlow 호출
+            }
+            else
+            {
+                CurrentPlayerTurn = (gameState - GameState.AI1Turn + 1);
+                AIs[CurrentPlayerTurn].FindOptimizedPath();
+                Callback();
+            }
         }
 
         /// <summary>
         /// AI의 턴에서 게임 화면에 게임 진행 상황을 표시하는 텍스트를 전달한다.
         /// </summary>
-        public Queue<string> SendGameLog()
+        public void SendGameLog()
         {
-            Queue<string> messages = null;
             if (CurrentPlayerTurn >= 0) // ai 턴일때만 CurrentPlayerTurn 값이 0 이상
             {
-                messages = AIs[CurrentPlayerTurn].MessageQueue;
+                Queue<string> messages = AIs[CurrentPlayerTurn].MessageQueue;
+                GameManager.Instance.SetGameLog(messages);
             }
-            return messages;
         }
 
         public void SetAIs(User[] players)
@@ -62,6 +59,7 @@ namespace RedTheSettlers.UnitTest
             for (int i = 0; i < AIs.Length; i++)
             {
                 AIs[i] = players[i + 1].GetComponent<BoardAI>();
+                AIs[i].AITurnEndCallBack = SendGameLog;
             }
         }
 
