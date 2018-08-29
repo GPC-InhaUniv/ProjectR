b9484cec4556216b8bd672b7953deefce02cb9a1
dahee <isc1645@gmail.com>
2018-08-29 14:06:48 +0900

    SRT-50 전투 관련 UI 스크립트 작성

---
+++ RedTheSettlers/Assets/2.Scripts/UI/UIBattleAttack.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+/// <summary>
+/// 작성자 : 강다희
+/// 공격, 스킬 공격 버튼 이벤트
+/// </summary>
+
+namespace RedTheSettlers.UI
+{
+    public class UIBattleAttack : MonoBehaviour
+    {
+        public enum AttackType
+        {
+            Attack,
+            Skill01,
+            Skill02,
+            Skill03,
+        }
+
+        public class UIAttackController : MonoBehaviour
+        {
+            [Header("Attack Button")]
+            [SerializeField]
+            private Button AttackButton;
+
+            [Header("Skill Attack Button")]
+            [SerializeField]
+            private Button SkillSlot1Button;
+
+            [SerializeField]
+            private Button SkillSlot2Button;
+
+            [SerializeField]
+            private Button SkillSlot3Button;
+
+            public void OnAttackButtonClick(AttackType attackType)
+            {
+                switch (attackType)
+                {
+                    case AttackType.Attack:
+                        {
+                            Debug.Log("Attack Click!"); //데이터 매니저에 저장되어 있는 어택 가져옴
+                        }
+                        break;
+
+                    case AttackType.Skill01:
+                        {
+                        }
+                        break;
+
+                    case AttackType.Skill02:
+                        {
+                        }
+                        break;
+
+                    case AttackType.Skill03:
+                        {
+                        }
+                        break;
+                }
+            }
+
+            private void Start()
+            {
+            }
+
+            private void Update()
+            {
+            }
+        }
+    }
+}
---
+++ RedTheSettlers/Assets/2.Scripts/UI/UIBattleItem.cs
@@ -0,0 +1,105 @@
+﻿using RedTheSettlers.GameSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 작성자 : 강다희
+/// 전투 중 아이템 사용 관련 UI
+/// [수정필요] 몬스터에 맞았을 때, HP가 줄어드는 계산 필요 (*승환님)
+/// </summary>
+
+namespace RedTheSettlers.UI
+{
+    public class UIBattleItem : MonoBehaviour
+    {
+        [Header("Player's Current Item")]
+        [SerializeField]
+        private Text HPItem;
+
+        [SerializeField]
+        private Text MPItem;
+
+        [SerializeField]
+        private Slider HPItemBar;
+
+        [SerializeField]
+        private Slider MPItemBar;
+
+        private int holdHPItem;
+        private int holdMPItem;
+
+        private int playerHP;
+        private int playerMP;
+        private float playerMaxHP;
+        private float playerMaxMP;
+
+        public void PutPlayerState()
+        {
+            PlayerData playerData = GameManager.Instance.gameData.PlayerData[0];
+
+            holdHPItem = playerData.ItemList[(int)ItemType.Cow].Count;
+            holdMPItem = playerData.ItemList[(int)ItemType.Water].Count;
+
+            HPItem.text = holdHPItem.ToString();
+            MPItem.text = holdMPItem.ToString();
+
+            playerHP = playerData.StatData.HealthPoint;
+            playerMaxHP = playerData.StatData.MagicPoint;
+
+            playerMP = playerData.StatData.MaxHealthPoint;
+            playerMaxMP = playerData.StatData.MaxMagicPoint;
+        }
+
+        public void SliderChanged()
+        {
+            HPItemBar.value = playerHP / playerMaxHP;
+            MPItemBar.value = playerMP / playerMaxMP;
+        }
+
+        public void OnUseItem(int ItemType)
+        {
+            Color textRedColor = new Color(255, 0, 0, 255);
+
+            if (ItemType == 0)
+            {
+                if (holdHPItem == 0)
+                {
+                    HPItem.color = textRedColor;
+                }
+                else if (playerHP < playerMaxHP)
+                {
+                    holdHPItem = holdHPItem - 1;
+                    HPItem.text = holdHPItem.ToString();
+                    playerHP = playerHP + 30;
+                    SliderChanged();
+                }
+            }
+            else if (ItemType == 1)
+            {
+                if (holdMPItem == 0)
+                {
+                    MPItem.color = textRedColor;
+                }
+                else if (playerMP < playerMaxMP)
+                {
+                    holdMPItem = holdMPItem - 1;
+                    MPItem.text = holdMPItem.ToString();
+                    playerMP = playerMP + 30;
+                    SliderChanged();
+                }
+            }
+        }
+
+        private void Start()
+        {
+            PutPlayerState();
+            SliderChanged();
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
